72c28dd57a5ac634d3ed8ba9d55ee89393b210a5
JoseParedesC <kbayro200@gmail.com>
2024-05-15 18:28:24 -0500

    main

--- funciones.js
+++ funciones.js
@@ -1,6 +1,6 @@
 const ER_Inicio = "^(Inicio)"
 const ER_Final = "(Fin)$"
-const declaraciones = "^(Declare) [a-z]*([ , (a-z)])* [ entero | real | cadena | logico ]*;$"
+const declaraciones = "^(Declare) [a-z]+([ , (a-z0-9)(_)?])* [entero|real|cadena|logico]+;$"
 var variables_globales = []
 
 
@@ -26,27 +26,32 @@ function ValidateDeclarations(){
     var texto_completo = document.getElementById("cuadrotexto").value
     let texto_completo_split = texto_completo.replace("Inicio", "").replace("Fin","").split("\n")
 
-    console.log(texto_completo_split)
+    texto_completo_split = texto_completo_split.filter(e => e.trim() !== "")
 
     let regExp = new RegExp(declaraciones)
 
     for(i = 0; i < texto_completo_split.length; i++){
         if(regExp.test(texto_completo_split[i])){
             variables = texto_completo_split[i]
-            variables = variables.replace("Declare","").replace(/[ entero | real | cadena | logico ]*;/, "").split(",")
+            variables = variables.replace("Declare","").replace(/[entero|real|cadena|logico]+;/, "").split(",")
             for(j = 0; j < variables.length; j++){
-                if(variables[j] =="")
-                    return false
+                if(variables[j] == "" || variables_globales.findIndex((el) => el == variables[j].trim()) != -1)
+                    return {"error":true, "msg":"La variable \""+variables[j].trim()+"\" declarada mas de 1 vez"}
                 else
-                    variables_globales.push(variables[j])
-                return true
+                    variables_globales.push(variables[j].trim())
             }
+        }else{
+            console.log(texto_completo_split)
+            return {"error":true, "msg":"Syntax error"}
         }
     }
+
+    return {"error":false, "msg":""};
 }
 
 
 function validateSyntaxis(){
+    variables_globales = []
     // if(validarInicioyFin()){        
 
     //     document.getElementById("msg_success").style.display = "block"
@@ -59,8 +64,8 @@ function validateSyntaxis(){
 
     // }
 
-
-    if(ValidateDeclarations()){        
+    let syn = ValidateDeclarations()
+    if(!syn.error){        
 
         document.getElementById("msg_success").style.display = "block"
         document.getElementById("msg_error").style.display = "none"
@@ -69,6 +74,8 @@ function validateSyntaxis(){
 
         document.getElementById("msg_success").style.display = "none"
         document.getElementById("msg_error").style.display = "block"
+        //document.getElementById("msg_error").innerHTML = syn.msg != "" && syn.msg
+        document.getElementById("msg_error").innerHTML = syn.msg != "" ? syn.msg : "La sintaxis es incorrecta"
 
     }
 }
